4fdc0ede00a1264dbfdaaf675f6bc8e180f61f35
ALip Maulana <makillerasli@gmail.com>
2025-02-14 14:35:32 +0700

    add modul tambah data

--- tests/Modul-Ubah-Presensi.spec.js
+++ tests/Modul-Ubah-Presensi.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
- test("Filter Data", async ({ page }) => {
+ test("Filter Data(TC001)", async ({ page }) => {
      test.setTimeout(60000);
      await page.goto('https://sim.dev.masook.id/#/login');
      await page.getByRole('textbox', { name: 'Username' }).click();
@@ -57,7 +57,85 @@ import { test, expect } from "@playwright/test";
      await page.locator('#toggleSelectDate').click();
      await page.getByRole('button', { name: 'Waktu April 2023 clear icon' }).click();
      await page.locator('div').filter({ hasText: /^Juni 2023$/ }).click();
+     await page.getByRole('button', { name: 'Terapkan' }).click();
     
-   
- 
    })
+
+   test("Tambah Data(TC002)", async ({ page }) => {
+    test.setTimeout(60000);
+    await page.goto('https://sim.dev.masook.id/#/login');
+    await page.getByRole('textbox', { name: 'Username' }).click();
+    await page.getByRole('textbox', { name: 'Username' }).fill('operatorjmi@mail.com');
+    await page.getByRole('textbox', { name: 'Kata Sandi' }).click();
+    await page.getByRole('textbox', { name: 'Kata Sandi' }).fill('111111');
+    await page.getByRole('button', { name: 'Masuk' }).click();
+    await expect(page).toHaveURL('https://sim.dev.masook.id/#/pilihOrganisasi');
+  
+    await page.getByText('Jayantara Indonesia').click();
+    await page.getByRole('link', { name: 'Ubah Presensi' }).click();
+    await page.locator('#add').click();
+    await page.getByRole('textbox', { name: 'Pilih Anggota' }).click();
+    await page.getByRole('option', { name: 'Alip test' }).click();
+    await page.getByRole('textbox', { name: 'Tanggal' }).click();
+    await page.getByRole('button', { name: '14' }).click();
+    await page.getByRole('textbox', { name: 'Keterangan' }).click();
+    await page.locator('[data-vv-name= "keterangan"]').fill('test absen');
+    await page.getByRole('button', { name: 'Simpan' }).click();
+    await page.locator('td:nth-child(8)').first().click();
+    await page.getByRole('menuitem', { name: 'Pratinjau' }).click();  
+    await page.waitForTimeout(3000);
+    await page.getByRole('button', { name: 'Tutup' }).click();
+
+    await page.locator('#add').click();
+    await page.getByRole('textbox', { name: 'Pilih Anggota' }).click();
+    await page.getByRole('option', { name: 'Alip test' }).locator('div').first().click();
+    await page.getByRole('textbox', { name: 'Tanggal' }).click();
+    await page.getByRole('button', { name: '14' }).click();
+    await page.getByRole('button', { name: 'Diisi jika akan terlambat' }).click();
+    await page.getByRole('option', { name: 'Skor 2 (31-60)' }).click();
+    await page.getByRole('button', { name: 'Diisi jika akan pulang cepat' }).click();
+    await page.getByRole('option', { name: 'Skor 4 (>90)' }).locator('div').first().click();
+    await page.getByRole('textbox', { name: 'Keterangan' }).click();
+    await page.locator('[data-vv-name= "keterangan"]').fill('test skor');
+    await page.getByRole('button', { name: 'Simpan' }).click();
+    await page.locator('tr:nth-child(3) > td:nth-child(8)').click();
+    await page.getByRole('menuitem', { name: 'Pratinjau' }).click();
+    await page.waitForTimeout(3000);
+    await page.getByRole('button', { name: 'Tutup' }).click();  
+
+    await page.locator('#add').click();
+    await page.getByRole('textbox', { name: 'Pilih Anggota' }).click();
+    await page.getByRole('option', { name: 'Alip test' }).click();
+    await page.getByRole('textbox', { name: 'Tanggal' }).click();
+    await page.getByText('2025', { exact: true }).click();
+    await page.getByText('1925', { exact: true }).click();
+    await page.getByRole('button', { name: 'Mar' }).click();
+    await page.getByRole('button', { name: '6', exact: true }).click();
+    await page.getByRole('textbox', { name: 'Keterangan' }).click();
+    await page.locator('[data-vv-name= "keterangan"]').fill('test masa lampau');
+    await page.getByRole('button', { name: 'Simpan' }).click();
+    await page.getByRole('row', { name: 'Jumat, 06-03-1925 Alip test' }).locator('#moreMenu').click();
+    await page.getByRole('menuitem', { name: 'Pratinjau' }).click();
+    await page.waitForTimeout(3000);
+    await page.getByRole('button', { name: 'Tutup' }).click();
+
+
+    await page.locator('#add').click();
+    await page.getByRole('textbox', { name: 'Pilih Anggota' }).click();
+    await page.getByRole('option', { name: 'Alip test' }).locator('div').first().click();
+    await page.getByRole('textbox', { name: 'Tanggal' }).click();
+    await page.getByText('2025', { exact: true }).click();
+    await page.getByText('2125').click();
+    await page.getByRole('button', { name: 'Februari', exact: true }).click();
+    await page.getByRole('cell', { name: 'W', exact: true }).click();
+    await page.getByRole('button', { name: '1', exact: true }).click();
+    await page.getByRole('textbox', { name: 'Keterangan' }).click();
+    await page.locator('[data-vv-name= "keterangan"]').fill('test masa depan');
+    await page.getByRole('button', { name: 'Simpan' }).click();
+    await page.getByRole('row', { name: 'Kamis, 01-02-2125 Alip test' }).locator('#moreMenu').click();
+    await page.getByRole('menuitem', { name: 'Pratinjau' }).click();
+    await page.waitForTimeout(3000);
+    await page.getByRole('button', { name: 'Tutup' }).click();  
+
+    
+  })
